42f55fb46b73911d1ef219246eab243b7fb6eebf
Shengwei Zhu <zhu.shengwei35@gmail.com>
2026-06-22 21:59:01 -0700

    Add create-ou.ps1

---
+++ proxmox-lab/create-ou.ps1
@@ -0,0 +1,8 @@
+# Replace the "szhu" and "lab" values with the name and extension of your domain
+New-ADOrganizationalUnit -Name "Executive" -Path "DC=szhu,DC=lab"
+New-ADOrganizationalUnit -Name "IT" -Path "DC=szhu,DC=lab"
+New-ADOrganizationalUnit -Name "Sales" -Path "DC=szhu,DC=lab"
+New-ADOrganizationalUnit -Name "Finance" -Path "DC=szhu,DC=lab"
+New-ADOrganizationalUnit -Name "HR" -Path "DC=szhu,DC=lab"
+New-ADOrganizationalUnit -Name "Marketing" -Path "DC=szhu,DC=lab"
+New-ADOrganizationalUnit -Name "Operations" -Path "DC=szhu,DC=lab"
